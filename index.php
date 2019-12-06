1c85fbbc63ea2e995b93ff70ca796e613928a89a
kimberlygarces <kimberly.gp@hotmail.com>
2019-12-06 10:07:47 -0500

    login

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Inicio</title>
+  <title>Infiltrometro</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
@@ -9,76 +9,53 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
     
+     
 <!--Animacion-->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css">
-
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css">
+ 
   <link href="css/Inicio.css" rel="stylesheet">
 
 
 </head>
 <body>
 
-<nav class="navbar navbar-expand-md bg-dark navbar-dark">
-  <!-- Brand -->
-  <a class="navbar-brand" href=Inicio.php>Infiltrometro</a>
-
-  <!-- Toggler/collapsibe Button -->
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <!-- Navbar links -->
-  <div class="collapse navbar-collapse" id="collapsibleNavbar">
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="">Salir</a>
-      </li>
-       </ul>
-  </div>
-</nav>
 <div class="jumbotron text-center">
-<img class="animated infinite fadeIn" src="img/user.png">
+  <h1>Uninfiltrometro</h1>
+</div>
+  
+<div class="container text-center">
+  <img class="animated infinite tada" src="img/user.png">
 
-   
+            <form class="form-signin">
+                <label for="inputEmail" class="sr-only">Usuario</label>
+                <input type="email" id="inputEmail" class="form-control" placeholder="Usuario" required autofocus>
+                <br>                    
+                <label for="inputPassword" class="sr-only">Contraseña</label>
+                <input type="password" id="inputPassword" class="form-control" placeholder="Contraseña" required>
+                <br>
+                <button class="btn btn-lg btn-secondary btn-block" type="submit">Inciar seción</button>
+                  
+            </form>
 </div>
-
-<div class="container">
-<div class="row justify-content-between">
-<!-- boton para acceder a la prueba en curso -->
-<div class="col-lg-4 col-sm-12">
-<ul class="list-unstyled list-inline py-2">
-  <li class="list-inline-item">
-  <button  onclick="location='Prueba_en_curso.php'" type="button"> <img class="animated infinite tada delay-2s" src="img/prueba.png" alt="x" /></button>
-  </li>
-  <li class="list-inline-item">
-  <h5 class="mb-1">Prueba en curso</h5>
-  </li>
-</ul>
-</div>
-<!-- boton para acceder al historial de pruebas -->
-<div class="col-lg-4 col-sm-12">
-<ul class="list-unstyled list-inline py-2">
-  <li class="list-inline-item">
-  <button onclick="location='historial.php'" type="submit"><img class="animated infinite tada delay-2s" src="img/historial.png" alt="x" /></button>
-  </li>
-  <li class="list-inline-item">
-  <h5 class="mb-1">Historial</h5>
-  </li>
-</ul>
-</div>
-
-</div>
-</div>
-         
 <!-- Footer -->
 <footer class="page-footer font-small unique-color-dark pt-4">
 
+  <!-- Footer Elements -->
+  <div class="container">
+
+    <!-- Call to action -->
+    <ul class="list-unstyled list-inline text-center py-2">
+      <li class="list-inline-item">
+        <h5 class="mb-1">Nuevo Usuario</h5>
+      </li>
+      <li class="list-inline-item">
+        <button  onclick="location='registro.php'" type="button" class="btn btn-outline-dark">Registrar</button>
+      </li>
+    </ul>
+
+  </div>
+
+  <!-- Copyright -->
   <div class="footer-copyright text-center py-3">© 2019: Ingenieria de Sistemas
     <a href="https://mdbootstrap.com/education/bootstrap/"> InfiltrometroUnicor</a>
   </div>
