87d5a8e52ee340b6b51da102f786218502793503
gabriel espitia <gaesge@hotmail.com>
2020-02-03 16:01:09 -0500

    prueba 2

--- prueba.php
+++ prueba.php
@@ -2,65 +2,47 @@
 
 
 
-    <div class="container">
-
-  
-      <div id="Mostrar"class="row align-items-start">
-
-      <div class="col-sm-12 col-lg-6">
-        <h1>Datos prueba</h1>
-     
+<div class="container">
+  <div id="Mostrar" class="row align-items-start">
+    <div class="col-sm-12 col-lg-6">
+      <h1>Datos prueba</h1>
       <table class="table table-striped">
-
-      <thead>
-      <th>PRUEBA</th>
+        <thead>
+          <th>PRUEBA</th>
           <th>N_DATO</th>
           <th> TIEMPO </th>
           <th> DISTANCIA </th>
-          </thead>
-          
-      <tbody id="myTable">
+        </thead>
 
-      <?php
-          if(!empty($_GET["Id"])){
-          //1.contectarse al servidor mysql
-          $conexion = mysqli_connect("localhost", "root", "") or die("ERROR: ".mysqli_error());
-          mysqli_select_db($conexion,"infiltrometro") or die("Error conectandose a la BD ");
-          //2.prepara una consulta sql
-          $sql = "SELECT * FROM historialdatos WHERE Id_Prueba = ".$_GET["Id"];
-          //3.ejecutar la consulta
+        <tbody id="myTable">
+          <?php
+          if (!empty($_GET["Id"])) {
+            //1.contectarse al servidor mysql
+            $conexion = mysqli_connect("localhost", "root", "") or die("ERROR: " . mysqli_error());
+            mysqli_select_db($conexion, "infiltrometro") or die("Error conectandose a la BD ");
+            //2.prepara una consulta sql
+            $sql = "SELECT * FROM historialdatos WHERE Id_Prueba = " . $_GET["Id"];
+            //3.ejecutar la consulta
 
-          $resultado = mysqli_query($conexion, $sql) or die("ERROR: ".mysqli_error($conexion));
-          if(empty($resultado)){
-            echo '<tr><td colspan="4"></td></tr>';
+            $resultado = mysqli_query($conexion, $sql) or die("ERROR: " . mysqli_error($conexion));
+            if (empty($resultado)) {
+              echo '<tr><td colspan="4"></td></tr>';
+            } else {
+              $i = 0;
+              while ($registro = mysqli_fetch_array($resultado)) {
+                echo '<tr>';
+                echo '<td>' . $registro["Id_Prueba"] . '</td>';
+                echo '<td>' . $registro["N_Dato"] . '</td>';
+                echo '<td>' . $registro["tiempo"] . '</td>';
+                echo '<td>' . $registro["distancia"] . '</td>';
+                echo '</tr>';
+              }
+            }
           }
-          else{
-            $i=0;
-          while($registro = mysqli_fetch_array($resultado)){
-            echo '<tr>';
-            echo '<td>'.$registro["Id_Prueba"].'</td>';
-            echo '<td>'.$registro["N_Dato"].'</td>';
-            echo '<td>'.$registro["tiempo"].'</td>';
-            echo '<td>'.$registro["distancia"].'</td>';
-            echo '</tr>';
-                
-          }
-        }
-    
-        }
-
-        
           ?>
-
-
-           
-      
-
         </tbody>
-        </table>
-
-
-        </div>
-        </div>
-      </body>
-      </html>
+      </table>
+    </div>
+  </div>
+  </body>
+  </html>
