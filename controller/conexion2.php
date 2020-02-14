3fa1a4574a603502ac1beecc0a0e5f0fe059b3a8
kimberlygarces <kimberly.gp@hotmail.com>
2020-02-14 09:56:14 -0500

    --

--- controller/conexion2.php
+++ controller/conexion2.php
@@ -1,13 +1,13 @@
 <?php
-// $server = 'localhost:3306';
-// $username = 'gabriel';
-// $password = 'clave';
-// $database = 'infiltrometro';
-
-$server = 'localhost';
-$username = 'root';
-$password = '';
+$server = 'localhost:3306';
+$username = 'gabriel';
+$password = 'clave';
 $database = 'infiltrometro';
+
+// $server = 'localhost';
+// $username = 'root';
+// $password = '';
+// $database = 'infiltrometro';
 
     $conn = new mysqli($server, $username, $password, $database);
 
