0364fac329a581de157f28cd0753498e271d3265
kimberlygarces <kimberly.gp@hotmail.com>
2020-03-13 10:45:15 -0500

    BD lOCALIZACIÓN INTENTO 3

--- controller/localizacion.php
+++ controller/localizacion.php
@@ -1,8 +1,8 @@
 <?php
 //1.contectarse al servidor mysql
 		require 'conexion2.php';
-
-		$sql = "INSERT INTO localizacion values (default,'".$_POST['Ciudad']."','".$_POST['Coordenadas']."','".$_POST['Observaciones']."')";
+		$sql =  "INSERT INTO `localizacion`(`Id_Prueba`, `ciudad`, `coordenadas`, `observaciones`) VALUES (default,'".$_POST['Ciudad']."','".$_POST['Coordenadas']."','".$_POST['Observaciones']."')";
+		// $sql = "INSERT INTO localizacion values (default,'".$_POST['Ciudad']."','".$_POST['Coordenadas']."','".$_POST['Observaciones']."')";
 		//3.ejecutar la consulta
 		$conn->query($sql);
 
