44a92b75e591fd783ff68ad35cc08826697b61e9
kimberlygarces <kimberly.gp@hotmail.com>
2020-03-13 10:39:37 -0500

    BD lOCALIZACIÓN INTENTO 3

--- controller/localizacion.php
+++ controller/localizacion.php
@@ -5,7 +5,7 @@
 		$sql = "INSERT INTO localizacion values (default,'".$_POST['Ciudad']."','".$_POST['Coordenadas']."','".$_POST['Observaciones']."')";
 		//3.ejecutar la consulta
 		$conn->query($sql);
-		$conn->close();        
-        header("Location: ../localizacion.php");
+
+		header("Location: ../localizacion.php");
 		
 ?>
