11e840e3c3d981bfcfc6d21aeba957b7b94268d9
kimberlygarces <kimberly.gp@hotmail.com>
2020-03-14 00:49:54 -0500

    probando historial BD

--- model/data.php
+++ model/data.php
@@ -3,18 +3,18 @@ require_once "prueba.php";
 //1.contectarse al servidor mysql
 require 'conexion2.php';
 
-		$conexion = mysqli_connect($server, $username, $password, $database) or die("ERROR : ".mysqli_error());
+		$conexion = mysqli_connect($server, $username, $password, $database) or die("ERROR 1: ".mysqli_error());
 		mysqli_select_db($conexion,"infiltrometro") or die("Error conectandose a la BD ");
 
         class Data{
             private $con;
             public function __construct(){
-                $conexion = mysqli_connect("localhost","root","","infiltrometro") or die("ERROR: ".mysqli_error($conexion));
+                $conexion = mysqli_connect("localhost","root","","infiltrometro") or die("ERROR 2: ".mysqli_error($conexion));
             }
             public function getPrueba(){
     
                 $prueba = array();
-                $conexion = mysqli_connect("localhost","root","","infiltrometro") or die("ERROR: ".mysqli_error($conexion));
+                $conexion = mysqli_connect("localhost","root","","infiltrometro") or die("ERROR 3: ".mysqli_error($conexion));
                 $sql = "SELECT * FROM datosprueba";
                 //EJECUTAR LA CONSULTA
               $res = mysqli_query($conexion, $sql) or die("ERROR: ".mysqli_error($conexion));
