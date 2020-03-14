bc4ba8b17dd175dff848dcac470626f6ba9fafa7
kimberlygarces <kimberly.gp@hotmail.com>
2020-03-14 00:56:01 -0500

    probando historial BD

--- model/data.php
+++ model/data.php
@@ -9,14 +9,18 @@ require 'conexion2.php';
         class Data{
             private $con;
             public function __construct(){
+                require 'conexion2.php';
                 $conexion = mysqli_connect($server, $username, $password, $database) or die("ERROR 2: ".mysqli_error($conexion));
             }
             public function getPrueba(){
     
                 $prueba = array();
+                require 'conexion2.php';
                 $conexion = mysqli_connect($server, $username, $password, $database) or die("ERROR 3: ".mysqli_error($conexion));
                 $sql = "SELECT * FROM datosprueba";
                 //EJECUTAR LA CONSULTA
+                require 'conexion2.php';
+
               $res = mysqli_query($conexion, $sql) or die("ERROR 4: ".mysqli_error($conexion));
                 while($reg = mysqli_fetch_array($res)){
                     $p = new datosprueba();
