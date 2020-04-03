6fd58b54226cbdd0b433f60571bebeba6da7cac9
kimberlygarces <kimberly.gp@hotmail.com>
2020-04-03 11:01:01 -0500

    ajustes PRUEBA TOTAL SQL

--- controller/pruebatotal.php
+++ controller/pruebatotal.php
@@ -1,7 +1,7 @@
 <?php
-
 		require 'conexion2.php';
-		$sql =  "INSERT INTO `Prueba`(`Id`, `Ciudad`, `TipoSuelo`, `Observaciones`) VALUES ('".$_POST['Id']."','".$_POST['Ciudad']."','".$_POST['TipoSuelo']."','".$_POST['Observaciones']."')";
+		$sql = "INSERT INTO `prueba`(`Id`, `localizacion`, `suelo`, `Observaciones`) values ('".$_POST['Id']."','".$_POST['Ciudad']."','".$_POST['TipoSuelo']."','".$_POST['Observaciones']."')";
+				
 		$conn->query($sql);
 		
         header("Location: ../inicio.php");
