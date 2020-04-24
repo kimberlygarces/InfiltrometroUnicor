cb4563a76874327130976ccda80a33757b417e22
gabriel espitia <gaesge@hotmail.com>
2020-04-24 18:29:27 -0500

    mejorado listenerpost.php

--- controller/listenerpost.php
+++ controller/listenerpost.php
@@ -5,7 +5,11 @@ if (!isset($_POST["d"]) || !isset($_POST["p"]) || !isset($_POST["m"])) {
 $mensaje_array = explode(';', $_POST['m']);
 
 require_once("conexion2.php");
-$sql = "INSERT INTO `datosprueba`(`N_Dato`, `Id_Dispositivo`, `Id_Prueba`, `tiempo`, `distancia`) VALUES (NULL, '".$_POST['d']."', '".$_POST['p']."', '".$mensaje_array[0]."', '".$mensaje_array[1]."')";
-$conn->query($sql);         
+for ($i = 0; $i < count($mensaje_array)/2; $i++) {
+    $sql = "INSERT INTO `datosprueba`(`N_Dato`, `Id_Dispositivo`, `Id_Prueba`, `tiempo`, `distancia`) VALUES (NULL, '".$_POST['d']."', '".$_POST['p']."', '".$mensaje_array[$i]."', '".$mensaje_array[$i+1]."')";
+    $conn->query($sql); 
+}
+$conn->close();
+        
 echo "datos insertados";
 ?>
