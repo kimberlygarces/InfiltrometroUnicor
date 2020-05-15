7924bbb8d5341424df192227adbb6c3b1370896e
kimberlygarces <kimberly.gp@hotmail.com>
2020-05-15 09:01:25 -0500

    img inficor

--- index.php
+++ index.php
@@ -179,7 +179,7 @@
         logrando visualizar los datos desde este sitio web.</p>
       </div>
       <div class="col-md-5">
-      <img src="img/infiltraci.png" class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500">
+      <img src="img/ilustraciónInfiltrometro.png" class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500">
       </div>
     </div>
 
